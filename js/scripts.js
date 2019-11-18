d19c43db6f479b7e762f58ba0f428a2b5b42cb5e
David Boedigheimer <stealth259@gmail.com>
2019-11-18 13:25:36 -0800

    Add for loop to look through each element in the result array and see if it needs a comma or not.

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,11 @@ var beepBoop = function(numberInput) {
 
   for (var i = 0; i <= numberInput; i++) {
     if (i % 3 === 0 && i != 0) {
-      returnOrder.push(" I'm sorry, Dave. I'm afraid I can't do that.")
+      returnOrder.push(" 'I'm sorry, Dave. I'm afraid I can't do that.'")
     } else if (i.toString().includes("1")) {
-      returnOrder.push(" Boop!")
+      returnOrder.push(" 'Boop!'")
     } else if (i.toString().includes("0")) {
-      returnOrder.push(" Beep!")
+      returnOrder.push(" 'Beep!'")
     } else {
       returnOrder.push(" " + i)
     }
@@ -23,6 +23,13 @@ $(document).ready(function() {
     var numberInput = parseInt($("input#number").val());
     var result = beepBoop(numberInput);
 
-    $("#result").empty().append(result + ",");
+    for (var i = 0; i < result.length; i++) {
+      console.log(result.length, i);
+      if (i >= result.length - 1) {
+        $("#result").append(result[i]);
+      } else {
+        $("#result").append(result[i] + ",");
+      }
+    }
   });
 });
