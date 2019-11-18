e2b5d8ed22bf5094fc2c61b430827a2abb38c1ca
David Boedigheimer <stealth259@gmail.com>
2019-11-18 11:11:26 -0800

    Add 2nd and third pass.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,14 @@ var beepBoop = function(numberInput) {
   var returnOrder = [];
 
   for (var i = 0; i <= numberInput; i++) {
-    if (i == 0) {
-      returnOrder.push(i)
+    if (i % 3 === 0 && i != 0) {
+      returnOrder.push(" I'm sorry, Dave. I'm afraid I can't do that.")
+    } else if (i.toString().includes("1")) {
+      returnOrder.push(" Boop!")
+    } else if (i.toString().includes("0")) {
+      returnOrder.push(" Beep!")
+    } else {
+      returnOrder.push(" " + i)
     }
   }
   return returnOrder
@@ -17,6 +23,6 @@ $(document).ready(function() {
     var numberInput = parseInt($("input#number").val());
     var result = beepBoop(numberInput);
 
-    $("#result").empty().append(result + ", ");
+    $("#result").empty().append(result + ",");
   });
 });
